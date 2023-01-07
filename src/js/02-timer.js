64e7a78808094191a92b764c399fe03e6fd66653
Anton Logvinenko <loav2236@gmail.com>
2023-01-07 02:51:19 +0200

    закончил ДЗ 9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,55 +2,70 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysX = document.querySelector('[data-days]');
+const hoursX = document.querySelector('[data-hours]');
+const minutesX = document.querySelector('[data-minutes');
+const secondsX = document.querySelector('[data-seconds');
 
-inputEl.addEventListener('input', onInputEl);
+startBtn.disabled = true;
 
-flatpickr('#datetime-picker', {
+const fp = flatpickr(inputEl, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     // console.log(selectedDates[0]);
+    const endDate = selectedDates[0];
+
+    console.log(endDate);
+
+    if (endDate <= new Date()) {
+      return window.alert('Please choose a date in the future');
+    }
+    startBtn.disabled = false;
+
+    // запуск отсчета времени
+    startBtn.addEventListener('click', onStartBtnClick);
+    function onStartBtnClick() {
+      const timerId = setInterval(() => {
+        const currentDay = new Date();
+        const timeShow = endDate - currentDay;
+        console.log(timeShow);
+
+        // Выключаем таймер если остается менее 1000 мс,
+        // если  поставить ноль будет отрицательная секунда
+        if (timeShow < 1000) {
+          clearInterval(timerId);
+          startBtn.disabled = true;
+        }
+        function convertMs(ms) {
+          // Number of milliseconds per unit of time
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
+
+          const days = Math.floor(ms / day);
+
+          const hours = Math.floor((ms % day) / hour);
+
+          const minutes = Math.floor(((ms % day) % hour) / minute);
+
+          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+          const timeX = { days, hours, minutes, seconds };
+
+          daysX.textContent = timeX.days.toString().padStart(2, 0);
+          hoursX.textContent = timeX.hours.toString().padStart(2, 0);
+          minutesX.textContent = timeX.minutes.toString().padStart(2, 0);
+          secondsX.textContent = timeX.seconds.toString().padStart(2, 0);
+          return timeX;
+        }
+        convertMs(timeShow);
+        console.log(convertMs(timeShow));
+      }, 1000);
+    }
   },
 });
-
-let endDate = null;
-function onInputEl() {
-  endDate = new Date(inputEl.value);
-
-  console.log('внутри функции', endDate);
-  return endDate;
-}
-onInputEl();
-console.log('глобальная видимость', endDate);
-
-// const currentDay = new Date();
-// let Q = endDate - currentDay;
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = Math.floor(ms / day);
-
-//   const hours = Math.floor((ms % day) / hour);
-
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
-// console.log(convertMs(ms));
-// console.log(convertMs(enableTime)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-
-// let x = 0;
-// function qq() {
-//   x = 10 + 20;
-// }
-// qq();
-// console.log(x);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Напиши скрипт, который при сабмите формы вызывает
+//  функцию createPromise(position, delay) столько раз,
+// сколько ввели в поле amount.
+// //  При каждом вызове передай ей номер создаваемого промиса (position)
+// //  и задержку учитывая введенную пользователем первую задержку (delay)
+// и шаг (step).
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-aio-3.2.5.min.js';
+console.log(Notify);
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onformElSubmit);
+
+function onformElSubmit(evt) {
+  evt.preventDefault();
+
+  const firstDelay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  const amount = Number(evt.currentTarget.elements.amount.value);
+
+  for (let i = 0; i <= amount - 1; i += 1) {
+    const position = i + 1;
+    const delay = firstDelay + step * i;
+
+    function makePromise(position, delay) {
+      const promise = new Promise((res, rej) => {
+        const shouldResolve = Math.random() > 0.3;
+        setTimeout(() => {
+          if (shouldResolve) {
+            res({ position, delay });
+          } else {
+            rej({ position, delay });
+          }
+        }, delay);
+      });
+      return promise;
+    }
+
+    makePromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
